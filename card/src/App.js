215de8bfbc0799ec22c9b8850b9394778afd2006
unknown <shambhavigoel13>
2022-03-10 14:28:09 +0530

    comit

---
+++ card/src/App.js
@@ -0,0 +1,12 @@
+import Cards from "./component/card";
+import Header from "./component/head";
+function App() {
+  return (
+    <>
+    <Header/>
+    <Cards name="Shambhavi"/>
+    </>
+  );
+}
+
+export default App;
---
+++ card/src/component/head.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Head extends Component {
+  render() {
+    return (
+      <header className="clearfix mt-4">
+        <h1>Scrollable List Card</h1>
+      </header>
+    );
+  }
+}
+
+export default Head;
